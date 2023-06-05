8457191d20bd08a096711794e15b30ef09a99359
toke <touko@testausserveri.fi>
2023-06-05 10:41:03 +0300

    chore: move utils to their own file

--- syncer.py
+++ syncer.py
@@ -1,49 +1,9 @@
 #!/bin/python3
-from getpass import getpass
 import sys
 import click
 from pyicloud import PyiCloudService
-from os.path import expanduser
-from os import environ
 from sync_service import SyncService
-
-
-USERNAME_CACHE_FILE = expanduser("~/.username")
-
-
-def get_input_or_fallback_file(prompt, fallback_file):
-    try:
-        f = open(fallback_file, "r")
-        username = f.read()
-        f.close()
-    except FileNotFoundError:
-        username = input(prompt)
-
-        f = open(fallback_file, "w")
-        f.write(username)
-        f.close()
-
-    return username
-
-
-def get_credinteals(password_already_saved: bool):
-    username = get_input_or_fallback_file("Email: ", USERNAME_CACHE_FILE)
-    if password_already_saved:
-        return username, ""
-    return username, getpass(f"Password for {username}: ")
-
-
-def ask_yes_or_no(question_string: str, prefer_true: bool) -> bool:
-    if prefer_true:
-        return input(question_string + "[Y/n]: ").lower() != "n"
-    return input(question_string + " [y/N]: ").lower == "y"
-
-
-def get_env(key: str) -> str:
-    try:
-        return environ[key]
-    except KeyError:
-        return ""
+from utils import *
 
 
 def authenticate_2fa(api: PyiCloudService) -> int:
